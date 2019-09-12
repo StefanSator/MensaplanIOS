f28b96d6bc00f9691bfd220712fed9b3103a834d
Stefan Sator <stefan.sator@web.de>
2019-09-12 15:04:24 +0200

    Added Segmented Control, so days can be selected

--- Mensaplan/MealTableViewController.swift
+++ Mensaplan/MealTableViewController.swift
@@ -8,30 +8,28 @@
 
 import UIKit
 
-class MealTableViewController: UITableViewController {
+class MealTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     //MARK: Properties
     var meals = [Meal]()
-    @IBOutlet var mealTableView: UITableView!
+    @IBOutlet weak var mealTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadMealData()
-        while meals.count == 0 {
-            // wait on rest api call
-        }
-        mealTableView.reloadData()
+        mealTableView.dataSource = self
+        mealTableView.delegate = self
+        loadMealData(weekDay: "mo")
     }
 
     //MARK: Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "MealTableViewCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
             fatalError("Error in trying to downcast UITableViewCell to Type: MealTableViewCell.")
@@ -45,59 +43,74 @@ class MealTableViewController: UITableViewController {
         return cell
     }
     
+    //MARK: Actions
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        clearAllMealData()
+        switch sender.selectedSegmentIndex {
+        case 0:
+            loadMealData(weekDay: "mo")
+        case 1:
+            loadMealData(weekDay: "di")
+        case 2:
+            loadMealData(weekDay: "mi")
+        case 3:
+            loadMealData(weekDay: "do")
+        case 4:
+            loadMealData(weekDay: "fr")
+        default:
+            fatalError("The selected Index in UISegmentedControl doesn't exist.")
+        }
+    }
+    
     //MARK: Private Functions
-    private func loadMealData() {
+    private func loadMealData(weekDay: String) {
         // Set up the http request with URLSession
         let session = URLSession.shared
         // Check the Request URL
-        guard let url = URL(string: "https://regensburger-forscher.de:9001/mensa/uni/fr") else {
-            print("The URL could not be resolved!")
-            return
+        guard let url = URL(string: "https://regensburger-forscher.de:9001/mensa/uni/\(weekDay)") else {
+            fatalError("The URL could not be resolved.")
         }
         // Make the request with URLSessionDataTask
         let task = session.dataTask(with: url, completionHandler: {
             (data, response, error) in
             // Check for error on client side
             guard error == nil else {
-                self.handleClientError(error)
-                return
+                fatalError("An Error occured on client side, while executing REST Call. Error: \(error!.localizedDescription)")
             }
-            /* Check for error on server side */
+            // Check for error on server side
             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                self.handleServerError(response)
-                return
+                fatalError("An Error occured on server side, while executing REST Call.")
             }
             // Parse response data to JSON
             do {
                 let json = try JSONSerialization.jsonObject(with: data!, options: []) as! NSArray
                 self.initializeMealArray(withArray: json)
             } catch let error {
-                print("JSON error: \(error.localizedDescription)")
+                fatalError("JSON error: \(error.localizedDescription)")
+            }
+            // Reload Table: UITask so i need to call reloadData() on Main-Thread
+            DispatchQueue.main.async {
+                self.mealTableView.reloadData()
             }
         })
+        // Start the Task
         task.resume()
+    }
+    
+    private func clearAllMealData() {
+        meals = [Meal]()
     }
     
     private func initializeMealArray(withArray: NSArray) {
         for object in withArray {
             guard let dictionary = object as? NSDictionary else {
-                print("An Error occurred while converting JSON Object to NSDictionary.")
-                return
+                fatalError("An Error occurred while converting Object to NSDictionary.")
             }
             guard let meal = Meal(dictionary: dictionary) else {
-                print("An Error occurred while trying to create a Meal Object.")
-                return
+                fatalError("An Error occurred while trying to create a Meal Object from a Dictionary.")
             }
             meals.append(meal)
         }
-    }
-    
-    private func handleClientError(_ error: Error?) {
-        print("An Error occurred on client side, while executing REST Call.")
-    }
-    
-    private func handleServerError(_ response: URLResponse?) {
-        print("An Error occurred on server side, while executing REST Call.")
     }
 
 }
